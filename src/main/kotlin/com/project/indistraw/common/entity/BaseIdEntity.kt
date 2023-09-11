2f4ce1f7b45ea8c613ef724a5f2b7c455253c997
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-11 10:29:25 +0900

    :sparkles: :: BaseIdEntity 생성

---
+++ src/main/kotlin/com/project/indistraw/common/entity/BaseIdEntity.kt
@@ -0,0 +1,13 @@
+package com.project.indistraw.common.entity
+
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseIdEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val idx: Int
+)
