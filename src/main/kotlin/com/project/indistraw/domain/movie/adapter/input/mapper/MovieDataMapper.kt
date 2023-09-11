7e856f9d1cd106fbd3dbc424656c3794c186845e
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-11 23:12:05 +0900

    :sparkles: :: toResponse mapper 추가

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/MovieDataMapper.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/MovieDataMapper.kt
@@ -1,22 +1,30 @@
 package com.project.indistraw.domain.movie.adapter.input.mapper
 
 import com.project.indistraw.domain.movie.adapter.input.data.request.CreateMovieRequest
+import com.project.indistraw.domain.movie.adapter.input.data.response.MovieResponse
 import com.project.indistraw.domain.movie.application.port.input.dto.CreateMovieDto
+import com.project.indistraw.domain.movie.application.port.input.dto.MovieDto
 import org.springframework.stereotype.Component
 
 @Component
 class MovieDataMapper {
 
-    fun toDto(createMovieRequest: CreateMovieRequest): CreateMovieDto =
+    fun toDto(request: CreateMovieRequest): CreateMovieDto =
         CreateMovieDto(
-            title = createMovieRequest.title,
-            description = createMovieRequest.description,
-            movieUrl = createMovieRequest.movieUrl,
-            thumbnailUrl = createMovieRequest.thumbnailUrl,
-            movieHighlight = createMovieRequest.movieHighlight,
-            director = createMovieRequest.director,
-            actor = createMovieRequest.actor,
-            clowdTrue = createMovieRequest.clowdTrue
+            title = request.title,
+            description = request.description,
+            movieUrl = request.movieUrl,
+            thumbnailUrl = request.thumbnailUrl,
+            movieHighlight = request.movieHighlight,
+            director = request.director,
+            actor = request.actor,
+            clowdTrue = request.clowdTrue
+        )
+
+    fun toResponse(dto: MovieDto): MovieResponse =
+        MovieResponse(
+            idx = dto.idx,
+            thumbnailUrl = dto.thumbnailUrl
         )
 
 }
