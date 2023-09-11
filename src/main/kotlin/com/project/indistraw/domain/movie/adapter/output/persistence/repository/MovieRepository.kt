97c9887bb352c83b87c734683bde337c8748394f
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-11 23:13:00 +0900

    :sparkles: :: findByGenre 메서드 추가

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/repository/MovieRepository.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/repository/MovieRepository.kt
@@ -1,6 +1,9 @@
 package com.project.indistraw.domain.movie.adapter.output.persistence.repository
 
+import com.project.indistraw.domain.movie.adapter.output.persistence.entity.Genre
 import com.project.indistraw.domain.movie.adapter.output.persistence.entity.MovieEntity
 import org.springframework.data.repository.CrudRepository
 
-interface MovieRepository: CrudRepository<MovieEntity, Int>
+interface MovieRepository: CrudRepository<MovieEntity, Int> {
+    fun findByGenre(genre: Genre): List<MovieEntity>
+}
