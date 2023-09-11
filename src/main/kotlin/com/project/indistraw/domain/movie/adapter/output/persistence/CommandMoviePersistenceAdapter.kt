a6a39a525b3dc0081bb4b5736b02538aa699ff8e
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-11 10:31:33 +0900

    :sparkles: :: movie port, adapter 추가

---
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/CommandMoviePersistenceAdapter.kt
@@ -0,0 +1,19 @@
+package com.project.indistraw.domain.movie.adapter.output.persistence
+
+import com.project.indistraw.domain.movie.adapter.output.persistence.mapper.MovieMapper
+import com.project.indistraw.domain.movie.adapter.output.persistence.repository.MovieRepository
+import com.project.indistraw.domain.movie.application.port.output.CommandMoviePort
+import com.project.indistraw.domain.movie.domain.Movie
+import org.springframework.stereotype.Component
+
+@Component
+class CommandMoviePersistenceAdapter(
+    private val movieRepository: MovieRepository,
+    private val movieMapper: MovieMapper
+): CommandMoviePort {
+
+    override fun createMovie(movie: Movie) {
+        movieRepository.save(movieMapper.toEntity(movie))
+    }
+
+}
