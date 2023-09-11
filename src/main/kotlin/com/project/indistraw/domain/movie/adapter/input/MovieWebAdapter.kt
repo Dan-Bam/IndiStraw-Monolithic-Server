99acb1b5bfe3fa8c55bc75cb8e37c78fa7ceaa96
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-11 23:13:09 +0900

    :sparkles: :: 영화 전체보기 api 추가

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/MovieWebAdapter.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/MovieWebAdapter.kt
@@ -1,13 +1,18 @@
 package com.project.indistraw.domain.movie.adapter.input
 
 import com.project.indistraw.domain.movie.adapter.input.data.request.CreateMovieRequest
+import com.project.indistraw.domain.movie.adapter.input.data.response.MovieResponse
 import com.project.indistraw.domain.movie.adapter.input.mapper.MovieDataMapper
+import com.project.indistraw.domain.movie.adapter.output.persistence.entity.Genre
 import com.project.indistraw.domain.movie.application.port.input.CreateMovieUseCase
+import com.project.indistraw.domain.movie.application.port.input.FindMovieListUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -15,12 +20,19 @@ import javax.validation.Valid
 @RequestMapping("/api/v1/movie")
 class MovieWebAdapter(
     private val movieDataMapper: MovieDataMapper,
-    private val createMovieUseCase: CreateMovieUseCase
+    private val createMovieUseCase: CreateMovieUseCase,
+    private val findMovieListUseCase: FindMovieListUseCase
 ) {
 
     @PostMapping
     fun createMovie(@RequestBody @Valid createMovieRequest: CreateMovieRequest): ResponseEntity<Void> =
         createMovieUseCase.execute(movieDataMapper.toDto(createMovieRequest))
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @GetMapping
+    fun findMovieList(@RequestParam("gener") genre: Genre?): ResponseEntity<List<MovieResponse>> =
+        findMovieListUseCase.execute(genre)
+            .map { movieDataMapper.toResponse(it) }
+            .let { ResponseEntity.ok(it) }
 
 }
