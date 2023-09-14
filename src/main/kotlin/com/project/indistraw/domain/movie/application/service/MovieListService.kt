b05912765824270e1834f690141e8216aa1047fd
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-14 23:25:17 +0900

    :sparkles: :: 영화 리스트 Service 구현

---
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/service/MovieListService.kt
@@ -0,0 +1,39 @@
+package com.project.indistraw.domain.movie.application.service
+
+import com.proejct.indistraw.domain.crowdfunding.application.common.annotation.ServiceWithReadOnlyTransaction
+import com.project.indistraw.domain.movie.adapter.output.persistence.entity.Genre
+import com.project.indistraw.domain.movie.application.port.input.MovieListUseCase
+import com.project.indistraw.domain.movie.application.port.input.dto.MovieDto
+import com.project.indistraw.domain.movie.application.port.input.dto.MoviePagingDto
+import com.project.indistraw.domain.movie.application.port.output.QueryMoviePort
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+
+@ServiceWithReadOnlyTransaction
+class MovieListService(
+    private val queryMoviePort: QueryMoviePort
+): MovieListUseCase {
+
+    override fun execute(pageable: Pageable, genre: String?): MoviePagingDto {
+        val pageRequest = PageRequest.of(pageable.pageNumber, pageable.pageSize, Sort.by(Sort.Order.desc("createdAt")))
+        val movieList = if (genre == null) {
+            queryMoviePort.findAll(pageRequest)
+        } else {
+            queryMoviePort.findAllByGenre(pageRequest, Genre.valueOf(genre))
+        }
+
+        val movieListDto = movieList.map{
+            MovieDto(
+                idx = it.idx,
+                thumbnailUrl = it.thumbnailUrl
+            )
+        }.toList()
+
+        return MoviePagingDto(
+            isLate = movieList.isLast,
+            list = movieListDto
+        )
+    }
+
+}
