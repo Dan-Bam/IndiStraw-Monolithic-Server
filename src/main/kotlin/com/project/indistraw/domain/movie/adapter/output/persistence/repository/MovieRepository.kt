a74079e06b958f6fae8d1d2c6d1df1a4f376d3cc
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-14 23:25:03 +0900

    :sparkles: :: 영화 리스트 page 정렬 기능 추가

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/repository/MovieRepository.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/repository/MovieRepository.kt
@@ -2,8 +2,12 @@ package com.project.indistraw.domain.movie.adapter.output.persistence.repository
 
 import com.project.indistraw.domain.movie.adapter.output.persistence.entity.Genre
 import com.project.indistraw.domain.movie.adapter.output.persistence.entity.MovieEntity
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface MovieRepository: CrudRepository<MovieEntity, Int> {
-    fun findByGenre(genre: Genre): List<MovieEntity>
+interface MovieRepository: JpaRepository<MovieEntity, Int> {
+
+    fun findAllByGenre(pageable: Pageable, genre: Genre?): Page<MovieEntity>
+
 }
