765753af57b80dd0e8e1a4e0832a72883c4af405
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-15 09:53:54 +0900

    :sparkles: :: 영화 디테일 dto 추가

---
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/data/response/MovieDetailResponse.kt
@@ -0,0 +1,16 @@
+package com.project.indistraw.domain.movie.adapter.input.data.response
+
+import com.project.indistraw.domain.movie.domain.Actor
+import com.project.indistraw.domain.movie.domain.Director
+
+data class MovieDetailResponse(
+    val title: String,
+    val description: String,
+    val movieUrl: String,
+    val thumbnailUrl: String,
+    val director: List<Director?>,
+    val actor: List<Actor?>,
+    val movieHighLight: List<String>,
+    val clowdTrue: Boolean,
+    val genre: List<String>?
+)
