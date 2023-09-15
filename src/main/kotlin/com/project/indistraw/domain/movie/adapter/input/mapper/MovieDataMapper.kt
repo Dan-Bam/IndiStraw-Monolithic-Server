e4f84bf28d2a2b9f7a3f23487a158cd5f0688561
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-15 22:28:10 +0900

    :sparkles: :: 영화 수정 dto mapper 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/MovieDataMapper.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/MovieDataMapper.kt
@@ -1,12 +1,14 @@
 package com.project.indistraw.domain.movie.adapter.input.mapper
 
 import com.project.indistraw.domain.movie.adapter.input.data.request.CreateMovieRequest
+import com.project.indistraw.domain.movie.adapter.input.data.request.UpdateMovieRequest
 import com.project.indistraw.domain.movie.adapter.input.data.response.MovieDetailResponse
 import com.project.indistraw.domain.movie.adapter.input.data.response.MoviePagingResponse
 import com.project.indistraw.domain.movie.adapter.input.data.response.MovieResponse
 import com.project.indistraw.domain.movie.application.port.input.dto.CreateMovieDto
 import com.project.indistraw.domain.movie.application.port.input.dto.MovieDetailDto
 import com.project.indistraw.domain.movie.application.port.input.dto.MoviePagingDto
+import com.project.indistraw.domain.movie.application.port.input.dto.UpdateMovieDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -14,6 +16,18 @@ class MovieDataMapper {
 
     fun toDto(request: CreateMovieRequest): CreateMovieDto =
         CreateMovieDto(
+            title = request.title,
+            description = request.description,
+            movieUrl = request.movieUrl,
+            thumbnailUrl = request.thumbnailUrl,
+            movieHighlight = request.movieHighlight,
+            director = request.director,
+            actor = request.actor,
+            clowdTrue = request.clowdTrue
+        )
+
+    fun toDto(request: UpdateMovieRequest): UpdateMovieDto =
+        UpdateMovieDto(
             title = request.title,
             description = request.description,
             movieUrl = request.movieUrl,
