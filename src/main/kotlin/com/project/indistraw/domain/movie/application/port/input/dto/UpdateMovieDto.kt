752ba312c3968f4ebdcbb8b237abbb77c98172ed
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-15 22:27:01 +0900

    :sparkles: :: 영화 수정 dto

---
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/port/input/dto/UpdateMovieDto.kt
@@ -0,0 +1,12 @@
+package com.project.indistraw.domain.movie.application.port.input.dto
+
+data class UpdateMovieDto(
+    val title: String,
+    val description: String,
+    val movieUrl: String,
+    val thumbnailUrl: String,
+    val movieHighlight: List<String>,
+    val director: List<Int>,
+    val actor: List<Int>,
+    val clowdTrue: Boolean
+)
