ab7d69ada69a64cfd078a8302361cf7f7aedebc5
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-16 13:24:51 +0900

    :sparkles: :: security 설정

--- src/main/kotlin/com/project/indistraw/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/project/indistraw/global/security/config/SecurityConfig.kt
@@ -52,6 +52,7 @@ class SecurityConfig(
             .mvcMatchers(HttpMethod.GET, "/api/v1/movie/{movie_id}").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ACCOUNT.name)
             .mvcMatchers(HttpMethod.PATCH, "/api/v1/movie/{movie_id}").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ACCOUNT.name)
             .mvcMatchers(HttpMethod.DELETE, "/api/v1/movie/{movie_id}").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ACCOUNT.name)
+            .mvcMatchers(HttpMethod.POST, "/api/v1/movie/history").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ACCOUNT.name)
 
             .mvcMatchers(HttpMethod.GET, "/").permitAll()
             .anyRequest().permitAll()
