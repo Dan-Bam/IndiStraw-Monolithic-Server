7c270e4fc8928de8fb35ffe35a830e18e2c63752
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-16 14:09:07 +0900

    :sparkles: :: findByAccount 메서드 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/application/port/output/QueryMovieHistoryPort.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/port/output/QueryMovieHistoryPort.kt
@@ -1,10 +1,12 @@
 package com.project.indistraw.domain.movie.application.port.output
 
+import com.project.indistraw.domain.account.domain.Account
 import com.project.indistraw.domain.movie.domain.Movie
 import com.project.indistraw.domain.movie.domain.MovieHistory
 
 interface QueryMovieHistoryPort {
 
     fun findByMovie(movie: Movie): MovieHistory?
+    fun findByAccount(account: Account): List<MovieHistory>?
 
 }
