ae37d4d9fef938d58ec7bb8742fb7aa2fb6108cd
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-16 14:08:53 +0900

    :sparkles: :: 영화 시청 기록 리스트 dto

---
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/port/input/dto/MovieHistoryDto.kt
@@ -0,0 +1,8 @@
+package com.project.indistraw.domain.movie.application.port.input.dto
+
+data class MovieHistoryDto(
+    val title: String,
+    val thumbnailUrl: String,
+    val historyTime: Float,
+    val movieIdx: Int
+)
