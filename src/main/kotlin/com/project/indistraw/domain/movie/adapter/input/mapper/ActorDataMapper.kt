bae3aaaea66fd75356e3773cf8c5d212d567ed8c
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-16 16:54:47 +0900

    :sparkles: :: toDto mapper 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/ActorDataMapper.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/ActorDataMapper.kt
@@ -1,7 +1,9 @@
 package com.project.indistraw.domain.movie.adapter.input.mapper
 
+import com.project.indistraw.domain.movie.adapter.input.data.request.CreateActorRequest
 import com.project.indistraw.domain.movie.adapter.input.data.response.ActorResponse
 import com.project.indistraw.domain.movie.application.port.input.dto.ActorDto
+import com.project.indistraw.domain.movie.application.port.input.dto.CreateActorDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,6 +14,12 @@ class ActorDataMapper {
             id = dto.id,
             name = dto.name,
             profileUrl = dto.profileUrl
+        )
+
+    fun toDto(createActorRequest: CreateActorRequest): CreateActorDto =
+        CreateActorDto(
+            name = createActorRequest.name,
+            profileUrl = createActorRequest.profileUrl
         )
 
 }
