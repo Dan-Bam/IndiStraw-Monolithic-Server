558ff4846c69632d955c1b0d4894535b7dcacdd9
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-17 21:25:21 +0900

    :recycle: :: toDto mapper 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/DirectorDataMapper.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/mapper/DirectorDataMapper.kt
@@ -1,6 +1,8 @@
 package com.project.indistraw.domain.movie.adapter.input.mapper
 
+import com.project.indistraw.domain.movie.adapter.input.data.request.CreateDirectorRequest
 import com.project.indistraw.domain.movie.adapter.input.data.response.DirectorResponse
+import com.project.indistraw.domain.movie.application.port.input.dto.CreateDirectorDto
 import com.project.indistraw.domain.movie.application.port.input.dto.DirectorDto
 import org.springframework.stereotype.Component
 
@@ -12,6 +14,12 @@ class DirectorDataMapper {
             id = dto.id,
             name = dto.name,
             profileUrl = dto.profileUrl
+        )
+
+    fun toDto(request: CreateDirectorRequest): CreateDirectorDto =
+        CreateDirectorDto(
+            name = request.name,
+            profileUrl = request.profileUrl
         )
 
 }
