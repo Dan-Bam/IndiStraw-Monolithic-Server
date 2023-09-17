797867518fa594b7fd1cbdfbe4d1516a57c7a4e1
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-17 16:51:13 +0900

    :sparkles: :: findByNameContaining 메서드 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/repository/DirectorRepository.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/repository/DirectorRepository.kt
@@ -3,4 +3,6 @@ package com.project.indistraw.domain.movie.adapter.output.persistence.repository
 import com.project.indistraw.domain.movie.adapter.output.persistence.entity.DirectorEntity
 import org.springframework.data.repository.CrudRepository
 
-interface DirectorRepository: CrudRepository<DirectorEntity, Int>
+interface DirectorRepository: CrudRepository<DirectorEntity, Int> {
+    fun findByNameContaining(name: String): List<DirectorEntity>?
+}
