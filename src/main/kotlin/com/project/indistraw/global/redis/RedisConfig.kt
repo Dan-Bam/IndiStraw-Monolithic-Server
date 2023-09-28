34166080b90a99ed1b75cbc3ad863620e2e5727c
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-28 23:39:42 +0900

    :sparkles: :: RedisConfig 추가

---
+++ src/main/kotlin/com/project/indistraw/global/redis/RedisConfig.kt
@@ -0,0 +1,32 @@
+package com.project.indistraw.global.redis
+
+import com.project.indistraw.global.redis.properties.RedisProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig(
+    private val redisProperties: RedisProperties
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, String> {
+        val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        return redisTemplate
+    }
+
+}
