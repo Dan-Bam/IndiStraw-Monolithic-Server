2ae3b907556fd2d52a7d328ee101547169bf2e54
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-04 20:46:57 +0900

    :sparkles: :: findByMovieIdxAndAccount 메서드 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/QueryMovieHistoryPersistenceAdapter.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/output/persistence/QueryMovieHistoryPersistenceAdapter.kt
@@ -1,6 +1,7 @@
 package com.project.indistraw.domain.movie.adapter.output.persistence
 
 import com.project.indistraw.domain.account.adapter.output.persistence.mapper.AccountMapper
+import com.project.indistraw.domain.account.adapter.output.persistence.repository.AccountRepository
 import com.project.indistraw.domain.account.domain.Account
 import com.project.indistraw.domain.movie.adapter.output.persistence.mapper.MovieHistoryMapper
 import com.project.indistraw.domain.movie.adapter.output.persistence.mapper.MovieMapper
@@ -8,14 +9,17 @@ import com.project.indistraw.domain.movie.adapter.output.persistence.repository.
 import com.project.indistraw.domain.movie.application.port.output.QueryMovieHistoryPort
 import com.project.indistraw.domain.movie.domain.Movie
 import com.project.indistraw.domain.movie.domain.MovieHistory
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class QueryMovieHistoryPersistenceAdapter(
     private val movieHistoryMapper: MovieHistoryMapper,
     private val movieMapper: MovieMapper,
     private val accountMapper: AccountMapper,
-    private val movieHistoryRepository: MovieHistoryRepository
+    private val movieHistoryRepository: MovieHistoryRepository,
+    private val accountRepository: AccountRepository
 ): QueryMovieHistoryPort {
 
     override fun findByMovieAndAccount(movie: Movie, account: Account): MovieHistory? {
@@ -26,6 +30,12 @@ class QueryMovieHistoryPersistenceAdapter(
     override fun findByAccount(account: Account): List<MovieHistory>? {
         val movieHistory = movieHistoryRepository.findByAccount(accountMapper.toEntity(account))
         return movieHistory?.map { movieHistoryMapper.toDomain(it)!! }
+    }
+
+    override fun findByMovieIdxAndAccount(idx: Long, accountIdx: UUID): MovieHistory? {
+        val account = accountRepository.findByIdOrNull(accountIdx)
+        val movieHistory = movieHistoryRepository.findByMovieIdxAndAccount(idx, account!!)
+        return movieHistory.let { movieHistoryMapper.toDomain(it) }
     }
 
 }
