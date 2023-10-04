ee4dbea0fc6c90f23d3aa293ce72126ac52a131a
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-04 20:46:20 +0900

    :sparkles: :: findByMovieIdxAndAccount 메서드 구현

--- src/main/kotlin/com/project/indistraw/domain/movie/application/port/output/QueryMovieHistoryPort.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/port/output/QueryMovieHistoryPort.kt
@@ -3,10 +3,12 @@ package com.project.indistraw.domain.movie.application.port.output
 import com.project.indistraw.domain.account.domain.Account
 import com.project.indistraw.domain.movie.domain.Movie
 import com.project.indistraw.domain.movie.domain.MovieHistory
+import java.util.UUID
 
 interface QueryMovieHistoryPort {
 
     fun findByMovieAndAccount(movie: Movie, account: Account): MovieHistory?
     fun findByAccount(account: Account): List<MovieHistory>?
+    fun findByMovieIdxAndAccount(idx: Long, accountIdx: UUID): MovieHistory?
 
 }
