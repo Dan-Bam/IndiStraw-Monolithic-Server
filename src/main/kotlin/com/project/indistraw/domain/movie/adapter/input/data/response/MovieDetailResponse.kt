9365ed3962e0a06b75a218f0e806c14c8a65ebc7
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-10 14:02:44 +0900

    :recycle: :: dto 필드 타입 변경

--- src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/data/response/MovieDetailResponse.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/adapter/input/data/response/MovieDetailResponse.kt
@@ -1,16 +1,16 @@
 package com.project.indistraw.domain.movie.adapter.input.data.response
 
 import com.project.indistraw.domain.movie.adapter.output.persistence.entity.Genre
-import com.project.indistraw.domain.movie.domain.Actor
-import com.project.indistraw.domain.movie.domain.Director
+import com.project.indistraw.domain.movie.application.port.input.dto.ActorDto
+import com.project.indistraw.domain.movie.application.port.input.dto.DirectorDto
 
 data class MovieDetailResponse(
     val title: String,
     val description: String,
     val movieUrl: String,
     val thumbnailUrl: String,
-    val director: List<Director?>,
-    val actor: List<Actor?>,
+    val director: List<DirectorDto>,
+    val actor: List<ActorDto>,
     val movieHighlight: List<String>,
     val crowdTrue: Boolean,
     val genre: Genre
--- src/main/kotlin/com/project/indistraw/domain/movie/application/service/MovieDetailService.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/service/MovieDetailService.kt
@@ -3,6 +3,8 @@ package com.project.indistraw.domain.movie.application.service
 import com.proejct.indistraw.domain.crowdfunding.application.common.annotation.ServiceWithReadOnlyTransaction
 import com.project.indistraw.domain.movie.application.exception.MovieNotFoundException
 import com.project.indistraw.domain.movie.application.port.input.MovieDetailUseCase
+import com.project.indistraw.domain.movie.application.port.input.dto.ActorDto
+import com.project.indistraw.domain.movie.application.port.input.dto.DirectorDto
 import com.project.indistraw.domain.movie.application.port.input.dto.MovieDetailDto
 import com.project.indistraw.domain.movie.application.port.output.QueryActorPort
 import com.project.indistraw.domain.movie.application.port.output.QueryDirectorPort
@@ -23,8 +25,20 @@ class MovieDetailService(
             description = movie.description,
             movieUrl = movie.movieUrl,
             thumbnailUrl = movie.thumbnailUrl,
-            director = movie.director.map { queryDirectorPort.findById(it) },
-            actor = movie.actor.map { queryActorPort.findById(it) },
+            director = movie.director.map { queryDirectorPort.findByIdOrNull(it)!!.let {
+                DirectorDto(
+                    idx = it.idx,
+                    name = it.name,
+                    profileUrl = it.profileUrl
+                )
+            } },
+            actor = movie.actor.map { queryActorPort.findByIdOrNull(it)!!.let {
+                ActorDto(
+                    idx = it.idx,
+                    name = it.name,
+                    profileUrl = it.profileUrl
+                )
+            } },
             movieHighlight = movie.movieHighlight,
             crowdTrue = movie.crowdTrue,
             genre = movie.genre
