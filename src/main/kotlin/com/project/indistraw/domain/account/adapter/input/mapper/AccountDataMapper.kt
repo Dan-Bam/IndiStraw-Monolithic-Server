256051056e715c00d2848c093d8f6e3ec42cfa0c
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-17 10:10:27 +0900

    :sparkles: :: 내가 출연한 작품 api

--- src/main/kotlin/com/project/indistraw/domain/account/adapter/input/mapper/AccountDataMapper.kt
+++ src/main/kotlin/com/project/indistraw/domain/account/adapter/input/mapper/AccountDataMapper.kt
@@ -4,10 +4,8 @@ import com.project.indistraw.domain.account.adapter.input.data.request.UpdateAcc
 import com.project.indistraw.domain.account.adapter.input.data.request.UpdateAddressRequest
 import com.project.indistraw.domain.account.adapter.input.data.request.UpdatePasswordRequest
 import com.project.indistraw.domain.account.adapter.input.data.response.AccountInfoResponse
-import com.project.indistraw.domain.account.application.port.input.dto.AccountInfoDto
-import com.project.indistraw.domain.account.application.port.input.dto.UpdateAccountInfoDto
-import com.project.indistraw.domain.account.application.port.input.dto.UpdateAddressDto
-import com.project.indistraw.domain.account.application.port.input.dto.UpdatePasswordDto
+import com.project.indistraw.domain.account.adapter.input.data.response.FilmographyResponse
+import com.project.indistraw.domain.account.application.port.input.dto.*
 import org.mapstruct.InjectionStrategy
 import org.mapstruct.Mapper
 import org.mapstruct.MappingConstants
@@ -24,5 +22,6 @@ interface AccountDataMapper {
     infix fun toDto(request: UpdateAddressRequest): UpdateAddressDto
     infix fun toDto(request: UpdateAccountInfoRequest): UpdateAccountInfoDto
     infix fun toResponse(dto: AccountInfoDto): AccountInfoResponse
+    infix fun toResponse(dto: FilmographyDto): FilmographyResponse
 
 }
