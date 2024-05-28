701bc6cb3e746387fb4ed4096ba845a9ffd3c5cd
KimGyeongsuuu <sk62628504@gmail.com>
2024-05-28 14:35:09 +0900

    add :: constructor

--- src/main/kotlin/com/project/indistraw/domain/movie/application/port/input/dto/MovieDto.kt
+++ src/main/kotlin/com/project/indistraw/domain/movie/application/port/input/dto/MovieDto.kt
@@ -3,4 +3,6 @@ package com.project.indistraw.domain.movie.application.port.input.dto
 data class MovieDto(
     val idx: Long,
     val thumbnailUrl: String
-)
+) {
+    constructor(): this(0L, "")
+}
