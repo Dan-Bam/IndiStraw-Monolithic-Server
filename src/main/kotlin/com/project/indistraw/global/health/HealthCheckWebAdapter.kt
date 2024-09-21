a056dc388e25dae34843c3aca902292acd0d75f9
KimGyeongsuuu <sk62628504@gmail.com>
2024-09-21 14:29:34 +0900

    test :: ci cd test

--- src/main/kotlin/com/project/indistraw/global/health/HealthCheckWebAdapter.kt
+++ src/main/kotlin/com/project/indistraw/global/health/HealthCheckWebAdapter.kt
@@ -9,6 +9,6 @@ class HealthCheckWebAdapter {
 
     @GetMapping
     fun healthCheck(): ResponseEntity<Map<String, String>> =
-        ResponseEntity.ok(mapOf("message" to "Indi-Straw server running"))
+        ResponseEntity.ok(mapOf("message" to "IndiStraw server running"))
 
 }
