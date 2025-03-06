588cc9322bb3d969f36c13f9aac8da64eb60580f
KimGyeongsuuu <sk62628504@gmail.com>
2025-03-06 11:00:39 +0900

    add :: monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,6 +89,11 @@ dependencies {
 
 	/* aws */
 	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
+	/* monitoring */
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+
 }
 
 dependencyManagement {
